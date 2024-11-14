78a1db578e90f6f69f3b106c4918cf011fff0102
shintaem2005 <shintaem95@gmail.com>
2024-11-14 16:17:20 +0700

    Update Navbar.js
    Update on path ( less complicated )

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,9 @@ const Navbar = () => {
     <nav>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li><Link to="bioscientive\src\pages\RegistrationForm.js">Registration</Link></li>
-        <li><Link to="bioscientive\src\pages\LoginForm.js">Login</Link></li>
-        <li><Link to="bioscientive\src\pages\Materi.js">Materi</Link></li>
+        <li><Link to="/bioscientive\src\pages\RegistrationForm.js">Registration</Link></li>
+        <li><Link to="/bioscientive\src\pages\LoginForm.js">Login</Link></li>
+        <li><Link to="/bioscientive\src\pages\Materi.js">Materi</Link></li>
         {/* <li><Link to="/">Home</Link></li>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/">Home</Link></li> */}
