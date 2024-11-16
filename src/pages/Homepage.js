6ad8f4cf32df431470efc14fedece569caafc85d
shintaem2005 <shintaem95@gmail.com>
2024-11-16 10:29:35 +0700

    Update Homepage.js
    Inserting basic codes
    import react
    const, return, div, h1, paragraph
    export default Homepage
    (className)

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,2 +1,14 @@
-// Dashboard adalah Homepage
+// Homepage adalah Dashboard
 
+import React from "react";
+
+const Homepage = () => {
+  return(
+    <div className="container text-center my-5">
+      <h1>Welcome to BIOSCIENTIVE</h1>
+      <p>Biology is Fun and Interesting</p>
+    </div>
+  );
+};
+
+export default Homepage;
