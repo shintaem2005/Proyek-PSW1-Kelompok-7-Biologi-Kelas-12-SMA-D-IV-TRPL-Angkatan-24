5f0b44a6ac2ada3a71d7cfc6b62ed43392e970d4
shintaem2005 <shintaem95@gmail.com>
2024-11-16 10:26:50 +0700

    Update Navbar.js 2
    Complete changes 
    
    import react and link
    
    putting nav, ul, li, link, button, and span (with className)
    
    export default Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,38 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/bioscientive\src\pages\RegistrationForm.js">Registration</Link></li>
-        <li><Link to="/bioscientive\src\pages\LoginForm.js">Login</Link></li>
-        <li><Link to="/bioscientive\src\pages\Materi.js">Materi</Link></li>
-        {/* <li><Link to="/">Home</Link></li>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/">Home</Link></li> */}
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+
+        <Link className="navbar-brand" to="/home">BIOSCIENTIVE</Link>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/home">Home</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">Registration</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">Login</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/materi">Materi</Link>
+            </li>
+
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
