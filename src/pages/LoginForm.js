a0cb08df5a8a49e5c6c966704f7c4d016d453970
shintaem2005 <shintaem95@gmail.com>
2024-11-19 16:20:21 +0700

    Perbaikan Login
    Kesalahan dalam penguploadan dari saya

--- src/pages/LoginForm.js
+++ src/pages/LoginForm.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
   const [credentials, setCredentials] = useState({
@@ -6,23 +7,37 @@ const LoginForm = () => {
     password: ''
   });
 
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+  
   const handleChange = (e) => {
     const { name, value } = e.target;
     setCredentials({
       ...credentials,
       [name]: value
     });
+    setError("");
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Logged in:', credentials);
+    if (credentials.email === "" && credentials.password === "") {
+      console.log('Logged in:', credentials);
+
+      navigate("/home");
+    }
+    else {
+      setError("invalid email or password");
+    }
   };
 
   return (
     <div className="container my-5">
       <form onSubmit={handleSubmit} className="border p-4 rounded bg-light">
         <h2 className="text-center">Login Form</h2>
+        {error && <p className="text-danger">{error}</p>}
+        
         <div className="mb-3">
           <input
           type="email"
