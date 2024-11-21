6555c532fb717a841b68f0431261ef1efc1573cb
shintaem2005 <shintaem95@gmail.com>
2024-11-21 15:46:40 +0700

    Update Navbar.js
    Adding Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,15 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
 
-        <Link className="navbar-brand" to="/home">BIOSCIENTIVE</Link>
+        <Link className="navbar-brand" to="/home">
+        <img 
+        src="/BIOSCIENTIVE Logo/logo-png.png"
+        alt="Logo"
+        width="50"
+        height="50"
+        className="logo img-fluid"
+        />
+        </Link>
 
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
