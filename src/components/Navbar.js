8eb5d38b5aabfc9b19039b40890479382e93e661
NahessonSgsNgl <sugosngl@gmail.com>
2024-11-21 16:39:54 +0700

    Update Navbar and Homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Navbar.css"; // Import file CSS untuk styling tambahan
+import "./Navbar.css"; 
 import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
