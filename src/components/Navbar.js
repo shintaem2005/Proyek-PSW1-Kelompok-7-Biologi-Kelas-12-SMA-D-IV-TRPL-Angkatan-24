6764c416d125cdc482e78087691e9b732cea65e8
shandra36 <shandranainggolan@gmail.com>
2024-11-28 15:44:25 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
 
-      <Link className="navbar-brand" to="/home">
-        <img 
-        src="/BIOSCIENTIVE Logo/logo-png.png"
-        alt="Logo"
-        width="50"
-        height="50"
-        className="logo img-fluid"
-        />
-        </Link>
+        <Link className="navbar-brand" to="/home">BIOSCIENTIVE</Link>
 
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -47,7 +39,7 @@ const Navbar = () => {
               </Link>
             </li>
             <li className="nav-item">
-            <Link className="nav-link hover-grow" to="/pembelahansel&pewarisansifat">
+            <Link className="nav-link hover-grow" to="/PembelahanseldanPewarisansifat">
                 Pembelahan sel dan Pewarisan Sifat
               </Link>
             </li>
