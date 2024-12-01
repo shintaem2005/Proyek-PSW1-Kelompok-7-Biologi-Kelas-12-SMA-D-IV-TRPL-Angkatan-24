39825e622357405526e2921905151aaf23a0628f
shintaem2005 <shintaem95@gmail.com>
2024-12-01 08:48:53 +0700

    Update Homepage.js
    Adding Card

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -34,6 +34,58 @@ const Homepage = () => {
         </div>
       </div>
     </div>
+
+  <section className="row align-items-center mt-4 bg-white p-4 shadow-sm rounded">
+
+        <div className="col-md-3">
+        <div className="card-container">
+            <img class="card-img-top" src="https://cdn.prod.website-files.com/62041b52ed8a5756a8361408/650b76b2a66ebc491e52d45f_abundance-2.jpg" alt="Card Enzim dan Metabolisme" />
+            <div class="card-content">
+              <h5 class="card-title">Enzim dan Metabolisme</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <button className="btn"><a href="./enzymemetabolism" className="btn">Go To Page</a>
+              </button>
+            </div>
+        </div>
+        </div>
+
+        <div className="col-md-3">
+        <div className="card-container">
+            <img class="card-img-top" src="https://cdn.prod.website-files.com/62041b52ed8a5756a8361408/650b76b2a66ebc491e52d45f_abundance-2.jpg" alt="Card Pembelahan Sel dan Pewarisan Sifat" />
+            <div class="card-content">
+              <h5 class="card-title">Pembelahan Sel dan Pewarisan Sifat</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <button className="btn"><a href="./PembelahanseldanPewarisansifat" className="btn">Go To Page</a>
+              </button>
+            </div>
+        </div>
+        </div>
+
+        <div className="col-md-3">
+        <div className="card-container">
+            <img class="card-img-top" src="https://cdn.prod.website-files.com/62041b52ed8a5756a8361408/650b76b2a66ebc491e52d45f_abundance-2.jpg" alt="Card Evolusi" />
+            <div class="card-content">
+              <h5 class="card-title">Evolusi</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <button className="btn"><a href="./evolusi" className="btn">Go To Page</a>
+                </button>
+            </div>
+        </div>
+        </div>
+
+        <div className="col-md-3">
+        <div className="card-container">
+            <img class="card-img-top" src="https://cdn.prod.website-files.com/62041b52ed8a5756a8361408/650b76b2a66ebc491e52d45f_abundance-2.jpg" alt="Card Bioteknologi" />
+            <div class="card-content">
+              <h5 class="card-title">Bioteknologi</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <button className="btn"><a href="./bioteknologi" className="btn">Go To Page</a>
+              </button>
+            </div>
+        </div>
+        </div>
+
+      </section>
   </div>
       );
       };
